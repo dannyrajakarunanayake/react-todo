59ba17c6469e5f631c498713d7680a7d3d7b1a8d
danny ferdi <mesh.ferdi@gmail.com>
2020-02-07 11:41:24 +1100

    commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import './App.css';
+import uuid from 'uuid';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Dinner with wife',
+        completed: true
+      },
+      {
+        id: uuid.v4(),
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  // Toggle complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) })
+  }
+
+  // Delete todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id
+      !== id)] });
+  }
+
+  //  Add Todo
+  AddTodo = (title) => {
+    const newTodo ={
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+          <div className="container">
+          <Header />
+          <AddTodo AddTodo={this.AddTodo}/>
+          <Todos  todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+          </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
